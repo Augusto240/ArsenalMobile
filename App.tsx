49d47d1e7922e783468a72dde60af20ca08eb2ba
Augusto240 <jose.aug18@outlook.com>
2025-04-23 15:19:24 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.customText}>Imagem do maior time do mundo:</Text>
+
+        <Image
+          source={require("./assets/arsenal-football-club-4096.png")}
+          style={styles.image}
+        />
+
+        <StatusBar
+          style="light"
+          backgroundColor="#517bb9"
+          translucent={false}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#bbdefb",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  customText: {
+    fontSize: 24,
+    color: "#0067b1",
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 20,
+    fontWeight: "bold",
   },
 });
